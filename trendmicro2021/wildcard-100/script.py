4bde5a36b216a423b4c7fa75f5af3b072a186c24
Omar2535 <omar2535@alumni.ubc.ca>
2021-09-25 11:27:44 -0700

    add not-a-bad-bucket

--- trendmicro2021/wildcard-100/script.py
+++ trendmicro2021/wildcard-100/script.py
@@ -11,9 +11,21 @@ with open("very_old_odd_file1.txt", "rb") as f:
         elif byte == b'\t':
             result += '1'
 
-result_split = [result[i:i+8] for i in range(0, len(result), 8)]
-print(result_split)
-breakpoint()
+result_split = [result[i:i+5] for i in range(0, len(result), 5)]
 
+result_stats_mapper = {}
+
+# Print out resulting string
+resulting_string = ""
+for bit_string in result_split:
+    if bit_string in result_stats_mapper:
+        result_stats_mapper[bit_string] += 1
+    else:
+        result_stats_mapper[bit_string] = 1
+    
+    resulting_string += bit_string + " "
+
+print(resulting_string)
+print(result_stats_mapper)
 
 print("Complete")
