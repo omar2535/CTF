1b6f59e62b4883faec99d446b27a29850215b8fe
Omar2535 <omar2535@alumni.ubc.ca>
2022-03-17 16:35:18 -0700

    clean up decoder script

--- picoctf2022/sidechannel/decoder.py
+++ picoctf2022/sidechannel/decoder.py
@@ -2,34 +2,26 @@ import os
 import subprocess
 import time 
 
-NUM_TRIALS = 5
-found_pins = []
-
 found_pin = ""
 for index in range(0, 8):
     times_taken = []
     for i in range(0, 10):
         PIN = found_pin + str(i) + (8 - len(found_pin) - 1) * '0'
-        print(f"Trying PIN: {PIN}")
+        print(f"Testing PIN: {PIN}\r", end='')
         
         start_time = time.time()
-        res = os.system(f"echo \"{PIN}\" | ./pin_checker")
+        res = os.popen(f"echo \"{PIN}\" | ./pin_checker").read()
         time_taken = time.time() - start_time
 
-        print(f"time taken: {time_taken}")
+        # print(f"time taken: {time_taken}")
         times_taken.append(time_taken)
 
     # Find outlier in times taken
-    # LOOK FOR PIN ACCEPTED STRING!
     max_time = max(times_taken)
-    print(times_taken.index(max_time))
+    # print(f"DIGIT AT POSITION {index}: {times_taken.index(max_time)}")
     found_pin += str(times_taken.index(max_time))
-    breakpoint()
+    # breakpoint()
 
-found_pins.append(found_pin)
-
-print(f"FOUND PINS: {found_pins}")
-print(f"Trying pins!")
-
-for pin in found_pins:
-    os.system(f"echo \"{pin}\" | ./pin_checker")
+print(f"PIN FOUND: {found_pin}. Trying~")
+res = os.popen(f"echo \"{found_pin}\" | ./pin_checker").read()
+print(res)
